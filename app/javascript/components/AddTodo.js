8b98cac1e40b008c79093be5ddab1378d4ba739f
miopanda <114249779+miopanda@users.noreply.github.com>
2023-07-23 18:02:00 +0900

    return文と各ライブラリのバージョンの調整

--- app/javascript/components/AddTodo.js
+++ app/javascript/components/AddTodo.js
@@ -1,23 +1,23 @@
-import React, { useState } from 'react'
-import axios from 'axios'
-import styled from 'styled-components'
-import { toast } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css'
-import { FiSend } from 'react-icons/fi'
-import { symbol } from 'prop-types'
+import React, { useState } from 'react';
+import axios from 'axios';
+import styled from 'styled-components';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { FiSend } from 'react-icons/fi';
+import { useNavigate } from 'react-router-dom';
 
 const InputAndButton = styled.div`
   display: flex;
   justify-content: space-between;
   margin-top: 20px;
-`
+`;
 
 const InputName = styled.input`
   font-size: 20px;
   width: 100%;
   height: 40px;
   padding: 2px 7px;
-`
+`;
 
 const Button = styled.button`
   font-size: 20px;
@@ -33,62 +33,80 @@ const Button = styled.button`
     opacity: 0.5;
     cursor: default;
   `}
-`
+`;
 
 const Icon = styled.span`
   display: flex;
   align-items: center;
   margin: 0 7px;
-`
+`;
 
-toast.configure()
+toast.configure();
 
-function AddTodo(props) {
+function AddTodo() {
   const initialTodoState = {
     id: null,
     name: "",
     is_completed: false
+  };
+
+  const [todo, setTodo] = useState(initialTodoState);
+  const navigate = useNavigate();
+
+  const notify = () => {
+    toast.success("Todo successfully created!", {
+      position: "bottom-center",
+      hideProgressBar: true
+    });
   }
 
-const [todo, setTodo] = useState[initialTodoState]
+  const handleInputChange = event => {
+    const { name, value } = event.target;
+    setTodo({ ...todo, [name]: value });
+  };
 
-const handleInputChange = event => {
-  const { name, value } =event.target;
-  setTodo({ ...todo, [name]: value })
-}
+  const saveTodo = () => {
+    var data = {
+      name: todo.name,
+    };
 
-const notify = () => {
-  toast.success('Todo successfully created!', {
-    position: 'bottom-center',
-    hideProgressBar: true
-  })
-}
-
-const saveTodo = () => {
-  var data = {
-    name: todo.name
-  }
-
-  axios.post('/api/v1/todos', data)
-  .then(resp => {
-    setTodo({
-      id: resp.data.id,
-      name: resp.data.name,
-      is_completed: resp.data.is_completed
+    axios.post('/api/v1/todos', data)
+    .then(resp => {
+      setTodo({
+        id: resp.data.id,
+        name: resp.data.name,
+        is_completed: resp.data.is_completed
+      });
+      notify();
+      navigate("/todos");
     })
-    notify()
-    props.history.push('/todos')
-  })
-  .catch(e => {
-    console.log(e)
-  })
-}
+    .catch(e => {
+      console.log(e)
+    })
+  };
 
   return (
-    <div>
-      AddTodo
-    </div>
+    <>
+      <h1>New Todo</h1>
+      <InputAndButton>
+        <InputName
+          type="text"
+          required
+          value={todo.name}
+          onChange={handleInputChange}
+          name="name"
+        />
+        <Button
+          onClick={saveTodo}
+          disabled={(!todo.name || /^\s*$/.test(todo.name))}
+        >
+          <Icon>
+            <FiSend />
+          </Icon>
+        </Button>
+      </InputAndButton>
+    </>
   )
 }
 
-export default AddTodo
+export default AddTodo;
